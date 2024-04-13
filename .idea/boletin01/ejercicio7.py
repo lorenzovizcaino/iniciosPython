75f895e2cbc176ca66179dc1f6fd190280a432b5
Antonio_Lorenzo <lorenzovizcaino@gmail.com>
2024-04-13 12:05:41 +0200

    13/04/24_12:05

--- .idea/boletin01/ejercicio7.py
+++ .idea/boletin01/ejercicio7.py
@@ -3,3 +3,8 @@ Ejercicio 7
 Suponiendo que hemos introducido una cadena por teclado que representa una frase
 (palabras separadas por espacios), realiza un programa que cuente cuantas palabras tiene.
 """
+
+cadena=input("Dame la cadena a comprobar: ")
+contador=0
+for i in cadena:
+
