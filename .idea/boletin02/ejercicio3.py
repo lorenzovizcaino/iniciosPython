40e36b73308253deb55da340bb898c281f03a1b5
Antonio Lorenzo <lorenzovizcaino@gmail.com>
2024-04-23 01:33:14 +0200

    23/04/24_1:32

--- .idea/boletin02/ejercicio3.py
+++ .idea/boletin02/ejercicio3.py
@@ -5,7 +5,7 @@ siguientes métodos para la clase:
  Un constructor, donde los datos pueden estar vacíos.
  Los setters y getters para cada uno de los atributos. Hay que validar las entradas de datos.
  mostrar(): Muestra los datos de la persona.
- esMayorDeEdad(): Devuelve un valor lógico indicando si es mayor de edad.
+ esMayorDeEdad(): Devuelve un valor lógico indicando si es mayor de edad.,
 """
 
 class Persona:
